ce462575ab02f13bb1345bd849fe512b442fc45c
Emerson <125415618+Bietio@users.noreply.github.com>
2026-05-29 19:01:35 -0300

    Removed isLoaded() func and changed class to trait

--- ResourceLoader.php
+++ ResourceLoader.php
@@ -14,110 +14,95 @@
 
 declare(strict_types=1);
 
-namespace Plugin;
+namespace MLGBlock\utils;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\Config;
-use Exception;
 
-final class ResourceLoader
+use pocketmine\utils\Config;
+use pocketmine\utils\MainLogger;
+
+/**
+ * Trait version test
+ */
+trait ResourceLoader
 {
 
-    const DIRECTORY = "dir";
-
-    const FILE = "file";
-
     /** @var PluginBase */
-    private static $plugin = null;
+    private static $plugin;
 
     /**
-     * @param string $file
+     * @param  string $file
      * @return bool
      */
-    public static function isPhar(string $file): bool
+    public static function isFilePhar(string $file): bool
     {
         return (pathinfo($file, PATHINFO_EXTENSION) === 'phar');        
     }
-    
-    /**
-     * @param PluginBase $plugin
-     * @param string $file
-     * @throws Exception
+
+    /** 
+     * @param  PluginBase $plugin
+     * @throws \Exception
      * @return void
      */
-    public static function init(PluginBase $plugin, string $file)
+    public function loadResource(PluginBase $plugin)
     {
         self::$plugin = $plugin;
+        $file = $this->getFile();
 
-        if(self::isPhar($file)) 
+        if(self::isFilePhar($file)) 
         {
-            $file = "phar://{$file}plugin.yml";
-        }
-
-        $file = new Config($file, Config::YAML);
-
-        $resources = (array) $file->get('resources', false);
+            $file = "phar://{$file}";
+        } 
         
+        $file = new Config($file . 'plugin.yml', Config::YAML);
+
+        $resources = $file->get('resources', false);
+
         if ($resources === false) 
-            throw new Exception("The key \"resources\" not is setted");
+            return MainLogger::getLogger()->error("Key 'resources' isn't setted in '{$plugin->getName()}'");
 
         foreach ($resources as $resource):
-            $path = self::getDataFile($resource);
+            $path     = (string) self::getDataFile($resource);
             $dirname  = (string) pathinfo($path, PATHINFO_DIRNAME);
-            $basename = (string) pathinfo($path, PATHINFO_BASENAME);
 
-            if (!self::isLoaded($dirname, self::DIRECTORY)) 
+            if (!is_dir($dirname)) 
             {
                 mkdir($dirname, 0777, true);
             } 
 
-            if (!self::isLoaded($path, self::FILE) && $basename !== ".") 
+            if (!is_file($path)) 
             {
                 if ($plugin->saveResource($resource) === false) 
-                    throw new Exception("File \"{$resource}\" not found in the resources folder");
+                    return MainLogger::getLogger()->error("Not found file '{$resource}' in resources of '{$plugin->getName()}'");
             }
         endforeach;
+
+        MainLogger::getLogger()->info(\pocketmine\utils\TextFormat::GREEN . "Resource files of '{$plugin->getName()}' loaded!");
     }
 
     /**
      * Return the file with data folder
-     * @param string $file
-     * @throws Exception
+     * @param ?string $file
+     * @throws \Exception
      * @return string
      */
-    public static function getDataFile(string $file = ''): string
+    public static function getDataFile(string $file = null): string
     {
         if (is_null(self::$plugin)) 
-            throw new Exception("Need init the ResourceLoader");
+            throw new \Exception("Need init the ResourceLoader");
 
-        return self::$plugin->getDataFolder() . "$file";
+        return self::$plugin->getDataFolder() . $file;
     }
     
     /**
      * @param string $file
-     * @param int $type
-     * @param array $default
-     * @param ?bool $correct
+     * @param int    $type
+     * @param array  $default
+     * @param ?bool  $correct
      * @return Config
      */
     public static function newConfig(string $file, int $type, array $default = [], bool &$correct = null)
     {
         return new Config($file, $type, $default, $correct);
-    }
-
-    /**
-     * @param string $resource
-     * @param string $type
-     * @return bool
-     */
-    public static function isLoaded(string $resource, string $type = self::DIRECTORY): bool
-    {
-        if (is_dir($resource) && $type === self::DIRECTORY) 
-            return true;
-
-        if (is_file($resource) && $type === self::FILE) 
-            return true;
-
-        return false;
     }
 }
